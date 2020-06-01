6ac5fe4f3c15746865018a2da822af0b33b90ddd
bayuaslamaa <bayuaslamaa@gmail.com>
2020-06-01 17:04:29 +0700

    redux done

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import NavigationPage from './src/containers/NavigationPage'
 import store from './src/store'
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+
+const baseUrl = 'http://127.1.0.0:3000'
 
 
 export const SET_QUEUES = 'SET_QUEUES'
@@ -9,3 +12,18 @@ export const setQueues = (data) => {
         payload: data
     }
 }
+
+const verifyId = (token) => {
+    return axios({
+        method: 'post',
+        url: baseUrl + '/verivy',
+        data: {
+            token
+        }
+    })
+}
+
+export const updateStatus = (id, status) => {
+
+}
+
